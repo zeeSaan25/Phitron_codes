37f840a5e31f603919097b128a179635c72817e7
Nayem Islam <128284940+zeeSaan25@users.noreply.github.com>
2023-11-24 00:07:06 +0600

    Update sort_object_using_builtin_sort_function.cpp
    normal rules and shortcut rules are given and all the descriptions are commented.

--- sort_object_using_builtin_sort_function.cpp
+++ sort_object_using_builtin_sort_function.cpp
@@ -11,12 +11,20 @@ class Student{
 };
 
 bool cmp(Student a, Student b){
-    if(a.marks > b.marks) return true;   //true return korle swap hobe nah, jmn ache setai thakbe
+    /*if(a.marks > b.marks) return true;   //true return korle swap hobe nah, jmn ache setai thakbe
     else if(a.marks < b.marks) return false;   //false return kora mane thik nai, swap korte hobe
-    else if(a.marks == b.marks){
-        if(a.roll <= b.roll) return true;
+    else{                     // if(a.marks == b.marks)
+        if(a.roll < b.roll) return true;
         else return false;
+    }*/
+
+    if(a.marks == b.marks){   //jodi 2 joner marks soman hoye jay tahole roll er vittite jar roll choto se age thakbe
+        return a.roll < b.roll;
     }
+    else{  //jodi marks soman na hoy tahole jar marks beshi se age asbe
+        return a.marks > b.marks;   //ami jeta cai condition e seta dite hobe
+    }
+
 }
 
 int main(){
